b8430ed24e101387fc55f245b900c0bb7851d7e0
Sasung Kim <sasungkim815@gmail.com>
2021-05-21 19:47:20 -0400

    until 23

--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import productForm
+
+
+def product_create_view(request):
+    form = productForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = productForm()
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
 
 
 def product_detail_view(request):
@@ -11,4 +23,9 @@ def product_detail_view(request):
     context = {
         'object': obj,
     }
-    return render(request, "products/detail.html", context)
+
+    # In App template
+    return render(request, "products/product_detail.html", context)
+
+    # From the main Templates folder
+    # return render(request,"product/detail.html", context)
