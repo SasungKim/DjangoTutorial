4212e8568efb2c38e5ea62f6b6ac97cfe50069f1
Sasung Kim <sasungkim815@gmail.com>
2021-05-25 23:13:04 -0400

    until 25

--- src/products/views.py
+++ src/products/views.py
@@ -1,26 +1,39 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import productForm
+from .forms import productForm, RawProductForm
 
 
-# HTML form (form only implemented in HTML only)
 def product_create_view(request):
+    my_form = RawProductForm()
     if request.method == "POST":
-        title = request.POST.get('title')
-        Product.objects.create(title=title)
-    context = {}
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
+    context = {
+        'form': my_form
+    }
     return render(request, "products/product_create.html", context)
 
+    # # HTML form (form only implemented in HTML only)
+    # def product_create_view(request):
+    #     if request.method == "POST":
+    #         title = request.POST.get('title')
+    #         Product.objects.create(title=title)
+    #     context = {}
+    #     return render(request, "products/product_create.html", context)
 
-# def product_create_view(request):
-#     form = productForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = productForm()
-#     context = {
-#         'form': form
-#     }
-#     return render(request, "products/product_create.html", context)
+    # def product_create_view(request):
+    #     form = productForm(request.POST or None)
+    #     if form.is_valid():
+    #         form.save()
+    #         form = productForm()
+    #     context = {
+    #         'form': form
+    #     }
+    #     return render(request, "products/product_create.html", context)
 
 
 def product_detail_view(request):
